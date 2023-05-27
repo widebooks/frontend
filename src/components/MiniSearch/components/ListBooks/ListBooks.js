6a0c5496e1affe72ff757b0d561c5d988d1cf452
Кравченко Вячеслав <kravchienkowork@gmail.com>
2023-05-27 23:11:41 +0400

    Add svg to empty list book

--- src/components/MiniSearch/components/ListBooks/ListBooks.js
+++ src/components/MiniSearch/components/ListBooks/ListBooks.js
@@ -1,11 +1,25 @@
+import cn from "classnames";
 import Book from "./component/Book";
 import cl from "./ListBooks.module.scss";
+
+import {ReactComponent as BookWithCrossDeleteSymbol} from "./static/bookWithCrossDeleteSymbol.svg";
 
 const ListBooks = ({
   books,
   pathBook,
   pathAvatar
 }) => {
+  if (!books.length) {
+    return (
+      <div className={cn(cl.list, cl.empty)}>
+        <BookWithCrossDeleteSymbol className={cl.empty__svg}/>
+        <div className={cl.empty__text}>
+          По заданным фильтрам нет подходящей книги. Можете посмотреть товар другого жанра или измениить название
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className={cl.list}>
       {books.map(b => (
